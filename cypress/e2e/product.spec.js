1fc3ea3ddb966c0742e99ed37667e67be8e91060
Mohammed Isioye <mohzak1103@gmail.com>
2024-08-28 09:36:57 +0100

    Update - Refactored product.spec.js - Refactored command.js files

--- cypress/e2e/product.spec.js
+++ cypress/e2e/product.spec.js
@@ -7,7 +7,7 @@ describe('Product Page tests', () => {
     });
     it('should search for a phone and purchase it', () => {
         cy.url().should('eq', 'https://ecommerce-playground.lambdatest.io/');
-        homePage.elements.searchBar().should('exist');
+        homePage.elements.searchBar().should('be.visible');
         homePage.elements.searchForm().eq(0).type('phone');
         homePage.elements.searchButton().eq(0).click();
         cy.url()
@@ -27,27 +27,7 @@ describe('Product Page tests', () => {
         cy.url().should('include', '/index.php?route=checkout/checkout');
 
         cy.fixture('user').then(user => {
-            cy.get('#input-payment-firstname').should('be.visible').type(user.firstName);
-            cy.get('#input-payment-lastname').should('be.visible').type(user.lastName);
-            cy.get('#input-payment-email').should('be.visible').type(user.emailAddress);
-            cy.get('#input-payment-telephone').should('be.visible').type(user.telephone);
-            cy.get('#input-payment-password').should('be.visible').type(user.passWord);
-
-            cy.get('#input-payment-confirm').should('be.visible').type(user.conFirm);
-            cy.get('#input-payment-company').should('be.visible').type(user.company);
-            cy.get('#input-payment-address-1').should('be.visible').type(user.address1);
-            cy.get('#input-payment-address-2').should('be.visible').type(user.address2);
-            cy.get('#input-payment-city').should('be.visible').type(user.city);
-            cy.get('#input-payment-postcode').should('be.visible').type(user.postcode);
-            cy.get('#input-comment').scrollIntoView().should('be.visible').type(user.comment);
-            cy.get('.sticky-top > :nth-child(3) > .custom-control-label')
-                .should('contain', 'I wish to subscribe to the Your Store newsletter.')
-                .click();
-            cy.get(':nth-child(4) > .custom-control-label')
-                .should('contain', 'I have read and agree to the ')
-                .click();
-            cy.get(':nth-child(6) > .custom-control-label').should('be.visible').click();
-            cy.get('#button-save').should('be.visible').click();
+            cy.fillPaymentForm(user);
         });
         cy.url().should('include', '/checkout/confirm');
         cy.contains('table > tfoot > tr:nth-child(5) > td:nth-child(2)', /[$]\d+\.\d{2}$/);
